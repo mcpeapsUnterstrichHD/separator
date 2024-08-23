1f84638b0b8b01b72adc6784b3b97cccf9ab89bc
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-08-23 04:27:08 +0200

    Update unit-test.sh

--- unit-test.sh
+++ unit-test.sh
@@ -2,29 +2,44 @@
 
 # The following should have successful output
 
-sep.sh
+echo "\x1b[32mThe following should have successful output\x1b[0m"
 
+echo "sep.sh"
+sep.sh
+echo "sep.sh --sep"
 sep.sh --sep =
+echo "sep.sh -s ..."
 sep.sh -s =
 sep.sh -s +-
 sep.sh -s \<\>-
 sep.sh -s output--
 sep.sh -s '- unit test 7 -'
 
+echo "sep.sh -box ..."
+
 sep.sh --box 1
+echo "sep.sh -b ..."
 sep.sh -b 2
-sep.sh -b 05 
+sep.sh -b 05
 sep.sh -b 8
 sep.sh -b 9
 
+echo "sep.sh -v"
 sep.sh -v
+echo "sep.sh --version"
 sep.sh --version
 
+echo "sep.sh -h"
 sep.sh -h
+echo "sep.sh -?"
 sep.sh -?
+echo "sep.sh --help"
 sep.sh --help
 
 # The following should have errors
+
+echo "\x1b[31mThe following should have errors\x1b[0m"
+
 
 sep.sh -x
 sep.sh 5
@@ -45,4 +60,3 @@ sep.sh -s = -b 4
 
 # sep.sh -h -v
 # sep.sh -v -h
-
